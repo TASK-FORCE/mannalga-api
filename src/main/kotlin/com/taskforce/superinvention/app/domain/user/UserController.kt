32dbb56826c205a2a5a21cf8d0bdfd7751d5f581
sdm3285@gmail.com <bksul1212@>
2020-08-03 19:49:54 +0900

    temp-save

--- src/main/kotlin/com/taskforce/superinvention/app/domain/user/UserController.kt
+++ src/main/kotlin/com/taskforce/superinvention/app/domain/user/UserController.kt
@@ -2,15 +2,12 @@ package com.taskforce.superinvention.app.domain.user
 
 import com.taskforce.superinvention.app.model.AppToken
 import com.taskforce.superinvention.app.web.dto.KakaoTokenDto
-import com.taskforce.superinvention.common.config.argument.auth.Authorize
-import com.taskforce.superinvention.common.config.security.JwtTokenProvider
 import com.taskforce.superinvention.common.config.security.SecurityUser
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 class UserController(
@@ -19,7 +16,7 @@ class UserController(
 
     @GetMapping("/profile")
     @PreAuthorize("isAuthenticated()")
-    fun getUserInfo(@Authorize auth: SecurityUser): User? {
+    fun getUserInfo(auth: SecurityUser): User? {
         return userService.getUserInfo(auth.user.userId)
     }
 
--- src/main/kotlin/com/taskforce/superinvention/common/config/argument/auth/Authorize.kt
+++
@@ -1,4 +0,0 @@
-package com.taskforce.superinvention.common.config.argument.auth
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-annotation class Authorize
