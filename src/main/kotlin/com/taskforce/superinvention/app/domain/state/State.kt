d199d22ba88d1f7cdbaf9ec4244c315b6bc43829
Jun Jeong <46917538+yyy9942@users.noreply.github.com>
2020-08-04 23:26:56 +0900

    state 조회  추가, 캐시로 저장해서 빠르게 조회 가능하도록

--- src/main/kotlin/com/taskforce/superinvention/app/domain/state/State.kt
+++ src/main/kotlin/com/taskforce/superinvention/app/domain/state/State.kt
@@ -1,15 +1,22 @@
 package com.taskforce.superinvention.app.domain.state
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.taskforce.superinvention.app.domain.BaseEntity
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 
 @Entity
 class State(
-        @ManyToOne
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JsonIgnore
         var superState: State?,
         var name: String,
         var superStateRoot: String,
-        var level: Long
+        var level: Long,
+
+        @OneToMany(mappedBy = "superState", fetch = FetchType.EAGER)
+        var subStates: List<State>
 ): BaseEntity() {
 }
---
+++ src/main/kotlin/com/taskforce/superinvention/app/domain/state/StateService.kt
@@ -0,0 +1,16 @@
+package com.taskforce.superinvention.app.domain.state
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+import java.util.stream.Collectors
+
+@Service
+class StateService(
+        var stateRepositorySupport: StateRepositorySupport
+) {
+    @Cacheable(cacheNames = arrayOf("cache"))
+    fun findAllStateDtoList(): List<StateDto> {
+        return stateRepositorySupport.findByLevel(1).stream().map { e -> StateDto(e) }.collect(Collectors.toList());
+    }
+}
