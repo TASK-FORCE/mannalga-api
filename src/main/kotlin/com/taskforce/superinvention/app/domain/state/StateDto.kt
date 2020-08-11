48fc21e1a4de280133669ffa3b18d74fd0cea4c8
Jun Jeong <46917538+yyy9942@users.noreply.github.com>
2020-08-11 20:55:50 +0900

    state의 seq가 안보이는 버그 수정

--- src/main/kotlin/com/taskforce/superinvention/app/domain/state/StateDto.kt
+++ src/main/kotlin/com/taskforce/superinvention/app/domain/state/StateDto.kt
@@ -4,13 +4,15 @@ import com.taskforce.superinvention.app.domain.BaseEntity
 import java.util.stream.Collectors
 
 
-class StateDto : BaseEntity {
+class StateDto {
+    var seq: Long?
     var name: String
     var superStateRoot: String
     var level: Long
     var subStates: List<StateDto>
 
     constructor(state: State) {
+        this.seq = state.seq
         this.name = state.name
         this.superStateRoot = state.superStateRoot
         this.level = state.level
