8eb990f5cb31d2ca5f48558bfe3fa7f9aa7fd114
SightStudio <sdm3285@gmail.com>
2020-09-17 23:32:08 +0900

    [temp] 에러 수정

--- src/main/kotlin/com/taskforce/superinvention/common/config/web/resttemplate/kakao/KakaoAuthResponseErrorHandler.kt
+++ src/main/kotlin/com/taskforce/superinvention/common/config/web/resttemplate/kakao/KakaoAuthResponseErrorHandler.kt
@@ -22,12 +22,16 @@ class KakaoAuthResponseErrorHandler: ResponseErrorHandler {
     }
 
     override fun handleError(response: ClientHttpResponse) {
-        LOG.error(response.toString())
+        val msg = "${response.statusCode}\n${response.rawStatusCode}\n${response.statusText}"
+        LOG.error(msg)
 
         when(response.statusCode.series()) {
-            // SERVER_ERROR -> {}
-            // CLIENT_ERROR -> {}
-            else -> throw Exception()
+            SERVER_ERROR -> {}
+            CLIENT_ERROR -> {}
+            else -> {
+                LOG.error("알 수 없는 에러입니다.")
+                throw Exception()
+            }
         }
     }
 }
