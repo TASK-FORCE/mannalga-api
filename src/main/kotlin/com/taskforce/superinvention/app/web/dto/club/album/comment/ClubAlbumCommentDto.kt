baef3ead1b80a8190a8d98e9be0f60312986adfb
SightStudio <sdm3285@gmail.com>
2020-11-15 20:34:42 +0900

    리뷰반영

--- src/main/kotlin/com/taskforce/superinvention/app/web/dto/club/album/comment/ClubAlbumCommentDto.kt
+++ src/main/kotlin/com/taskforce/superinvention/app/web/dto/club/album/comment/ClubAlbumCommentDto.kt
@@ -1,7 +1,9 @@
 package com.taskforce.superinvention.app.web.dto.club.album.comment
 
 import com.taskforce.superinvention.app.domain.club.album.comment.ClubAlbumComment
+import com.taskforce.superinvention.common.exception.BizException
 import com.taskforce.superinvention.common.util.extendFun.toBaseDateTime
+import org.springframework.http.HttpStatus
 
 data class ClubAlbumCommentRegisterDto(
         val content: String = ""
@@ -16,7 +18,8 @@ data class ClubAlbumCommentListDto(
     constructor(clubAlbumComment: ClubAlbumComment) :this(
             content          = clubAlbumComment.content,
             writer           = clubAlbumComment.clubUser.user.userName ?: "",
-            writeClubUserSeq = clubAlbumComment.clubUser.seq ?: 0,
-            registerTime     = clubAlbumComment.createdAt?.toBaseDateTime() ?: ""
+            registerTime     = clubAlbumComment.createdAt?.toBaseDateTime() ?: "",
+            writeClubUserSeq = clubAlbumComment.clubUser.seq
+                    ?: throw BizException("작성자를 확인할 수 없습니다.", HttpStatus.NOT_FOUND)
     )
 }
