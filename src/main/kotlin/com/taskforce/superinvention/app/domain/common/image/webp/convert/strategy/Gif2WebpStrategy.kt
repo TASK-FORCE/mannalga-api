dc46465bb9089be8002685286fc6b5989f7f9c48
SightStudio <sdm3285@gmail.com>
2021-04-11 10:50:12 +0900

    이미지 webp변환 실패시 exception handling 추가

--- src/main/kotlin/com/taskforce/superinvention/app/domain/common/image/webp/convert/strategy/Gif2WebpStrategy.kt
+++ src/main/kotlin/com/taskforce/superinvention/app/domain/common/image/webp/convert/strategy/Gif2WebpStrategy.kt
@@ -8,13 +8,20 @@ import com.taskforce.superinvention.app.domain.common.image.webp.convert.WebpCom
 import com.taskforce.superinvention.app.domain.common.image.webp.convert.handler.WebpAnimatedWriter
 import com.taskforce.superinvention.common.util.file.image.gif.GifMo
 import org.apache.commons.io.FileUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 
 @Service
 class Gif2WebpStrategy: WebpConvertStrategy {
+
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(Gif2WebpStrategy::class.java)
+    }
 
     override fun identify(imageFormat: ImageFormat): Boolean {
         return imageFormat == ImageFormat.GIF
@@ -23,16 +30,21 @@ class Gif2WebpStrategy: WebpConvertStrategy {
     override fun convert(fileName: String, file: File, param: WebpCompressionParam): File {
         val metadata = ImageMetadataReader.readMetadata(file)
 
-        val bytes = if(GifMo.isAnimated(metadata)) {
-            WebpAnimatedWriter.DEFAULT.writeAsByteArray(file)
-        } else {
-            ImmutableImage.loader()
-                .fromFile(file)
-                .bytes(WebpWriter.DEFAULT)
-        }
+        return try {
+            val bytes = if(GifMo.isAnimated(metadata)) {
+                WebpAnimatedWriter.DEFAULT.writeAsByteArray(file)
+            } else {
+                ImmutableImage.loader()
+                    .fromFile(file)
+                    .bytes(WebpWriter.DEFAULT)
+            }
 
-        val convertedFile = File("${fileName}.webp")
-        FileUtils.writeByteArrayToFile(convertedFile, bytes)
-        return convertedFile
+            val convertedFile = File("${fileName}.webp")
+            FileUtils.writeByteArrayToFile(convertedFile, bytes)
+            return convertedFile
+        } catch (e: IOException) {
+            LOG.error("GIF -> WebP간 변환 실패, 컨버팅을 취소합니다. {}", e)
+            file
+        }
     }
 }
