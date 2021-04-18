5f7c3ea7b8428a10c78ec5eb09a3202d7be7ed89
Jun Jeong <46917538+yyy9942@users.noreply.github.com>
2021-04-18 20:10:17 +0900

    UNAUTHRIZED => FORBIDEN  변경

--- src/main/kotlin/com/taskforce/superinvention/common/exception/club/album/NoAuthForClubAlbumException.kt
+++ src/main/kotlin/com/taskforce/superinvention/common/exception/club/album/NoAuthForClubAlbumException.kt
@@ -11,5 +11,5 @@ class NoAuthForClubAlbumException(
     constructor(): this("""
         |해당 모임 사진첩 사진에 대한 권한이 없습니다.
         |매니저, 마스터 그리고 사진 등록자만 요청할 수 있습니다.
-    """.trimMargin(), HttpStatus.UNAUTHORIZED)
+    """.trimMargin(), HttpStatus.FORBIDDEN)
 }
