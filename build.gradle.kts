ed44ac5a390c7b79864a18563559781506ab0cf0
SightStudio <sdm3285@gmail.com>
2021-04-22 01:24:28 +0900

    kotlin annotation all-open 롤백

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,6 @@ plugins {
     kotlin("plugin.noarg")    version "1.4.21"
 }
 
-allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.Embeddable")
-    annotation("javax.persistence.MappedSuperclass")
-}
-
 group = "com.taskforce"
 version = "SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
